13cd2ccc50c5703e1376aecbe64c0f934216fff3
Pedro Barauna <81633121+pedrobarauna8@users.noreply.github.com>
2023-11-09 22:23:40 -0300

    Add files via upload

--- js/curricularComponents.js
+++ js/curricularComponents.js
@@ -1,30 +1,49 @@
 class CurricularComponents extends React.Component {
+   constructor(props) {
+      super(props) 
+      this.state = { 
+         componentes: [
+            { id: 1, disciplina: 'Sistemas Operacionais', carga: 66.7, periodo: 'Noturno' },
+            { id: 2, disciplina: 'Inglês', carga: 33.3, periodo: 'Noturno' },
+            { id: 3, disciplina: 'Laboratório de Estruturas de Dados e Programação', carga: 33.3, periodo: 'Noturno' },
+            { id: 4, disciplina: 'Introdução a Banco de Dados', carga: 66.7, periodo: 'Noturno' }
+         ]
+      }
+   }
 
-  constructor() {
-    super();
-    this.state = { 
-      dictionary: {"Disciplina": "disciplina", "Carga": "carga", "Periodo": "periodo"}
-    };
-  }
+    renderTableHeader() {
+      let header = Object.keys(this.state.componentes[0]).splice(1,3);      
+      return header.map((key, index) => {
+         return <th key={index}>{key.toUpperCase()}</th>
+      })
+   }
 
-  render(){
-    return (
-      <div>
-        asdas
-      </div>
-      /*
-      <div>
-        <ul>
-        {Object.keys(dictionary).map(key => (
-          <li key={key}>
-            <strong>{key}:</strong> {dictionary[key]}
-          </li>
-        ))}
-      </ul>
-      </div>
-      */
-    )
-  }
+   renderTableData() {
+      return this.state.componentes.map((componente, index) => {
+         const { id, disciplina, carga, periodo } = componente //destructuring
+         return (
+            <tr key={id}>               
+               <td>{disciplina}</td>
+               <td>{carga}</td>
+               <td>{periodo}</td>
+            </tr>
+         )
+      })
+   }
+
+   render() { 
+      return (
+         <div>
+            <h2 id='titulo'>Componentes Curriculares</h2>
+            <table id='componentes'>
+               <tbody>
+                  <tr>{this.renderTableHeader()}</tr>
+                  {this.renderTableData()}
+               </tbody>
+            </table>
+         </div>
+      )
+   }
 }
 
-ReactDOM.render(<CurricularComponents />,document.getElementById('curricularComponents'));
+ReactDOM.render(<CurricularComponents />, document.getElementById('curricularComponents'));
